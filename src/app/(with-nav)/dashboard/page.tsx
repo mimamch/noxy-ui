4ae4f37b510f62e5a5f02153a6400353f62f9c86
Muhammad Imam Choirudin <mimamch28@gmail.com>
2023-05-28 21:14:37 +0700

    fix some bug

--- src/app/(with-nav)/dashboard/page.tsx
+++ src/app/(with-nav)/dashboard/page.tsx
@@ -22,10 +22,10 @@ export default function page() {
   return (
     <div className=" flex-col md:flex ">
       <div className="flex-1 space-y-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
+        <div className="flex flex-wrap items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-          <div className="flex items-center space-x-2">
-            <CalendarDateRangePicker />
+          <div className="flex  gap-y-2  flex-wrap items-center ">
+            <CalendarDateRangePicker className="mr-2" />
             <Button size="sm">
               <Download className="mr-2 h-4 w-4" />
               Download
--- src/app/(with-nav)/orders/page.tsx
+++ src/app/(with-nav)/orders/page.tsx
@@ -27,7 +27,7 @@ export default async function TaskPage() {
 
   return (
     <>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="h-full flex-1 flex-col space-y-8 p-8 flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
